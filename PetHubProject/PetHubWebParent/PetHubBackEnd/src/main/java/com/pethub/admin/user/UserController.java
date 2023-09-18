dd74afec0b026f6bd44979a493059a0e6eb21efc
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-09-18 23:34:41 +0700

    Update edit User information function

--- PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/user/UserController.java
+++ PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -16,31 +17,48 @@ import com.pethub.common.entity.User;
 public class UserController {
 	@Autowired
 	private UserService service;
-	
-	@GetMapping("/users") //handling HTTP get request
+
+	@GetMapping("/users") // handling HTTP get request
 	public String listAll(Model model) {
 		List<User> listUsers = service.listAll();
 		model.addAttribute("listUsers", listUsers);
-		return "users"; //return a logical view name to physical view file
+		return "users"; // return a logical view name to physical view file
 	}
-	
+
 	@GetMapping("/users/new")
 	public String newUser(Model model) {
 		List<Role> listRoles = service.listRoles();
 		User user = new User();
 		user.setEnabled(true);
-		model.addAttribute("user",user);
-		model.addAttribute("listRoles",listRoles);
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("pageTitle", "Create New User");
 		return "user_form";
 	}
-	
-	@PostMapping("/users/save") //handling HTTP post request
+
+	@PostMapping("/users/save") // handling HTTP post request
 	public String saveUser(User user, RedirectAttributes redirectAttributes) {
 		System.out.println(user);
 		service.save(user);
-		
+
 		redirectAttributes.addFlashAttribute("message", "The user has been saved succesfully.");
-		
+
 		return "redirect:/users"; // = respond.sendRedirect
+	}
+
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes redirectAttributes) { // Spring
+		try {
+			User user = service.get(id);
+			model.addAttribute("user", user);
+			List<Role> listRoles = service.listRoles();
+			model.addAttribute("listRoles", listRoles);
+
+			model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+			return "user_form";
+		} catch (UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+			return "redirect:/users";
+		}
 	}
 }
--- PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/user/UserService.java
+++ PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/user/UserService.java
@@ -1,8 +1,10 @@
 package com.pethub.admin.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -40,7 +42,15 @@ public class UserService {
 
 	public boolean isEmailUnique(String email) {
 		User userByEmail = userRepo.getUserByEmail(email);
-		
+
 		return userByEmail == null; // if return true == user is unique and so on
+	}
+
+	public User get(Integer id) throws UserNotFoundException {
+		try {
+			return userRepo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			throw new UserNotFoundException("Could not find any user with ID: " + id);
+		}
 	}
 }

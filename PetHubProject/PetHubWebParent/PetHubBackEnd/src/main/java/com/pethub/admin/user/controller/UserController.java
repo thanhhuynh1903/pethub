ba10484be8079923d72d57fa36f6d02e42762fb8
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-09-26 03:02:27 +0700

    refactor

--- PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/user/controller/UserController.java
+++ PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/user/controller/UserController.java
@@ -68,7 +68,7 @@ public class UserController {
 		model.addAttribute("reverseSortDir", reverseSortDir);
 		model.addAttribute("keyword", keyword);
 
-		return "users";
+		return "users/users";
 	}
 
 	@GetMapping("/users/new")
@@ -79,7 +79,7 @@ public class UserController {
 		model.addAttribute("user", user);
 		model.addAttribute("listRoles", listRoles);
 		model.addAttribute("pageTitle", "Create New User");
-		return "user_form";
+		return "users/user_form";
 	}
 
 	@PostMapping("/users/save") // handling HTTP post request
@@ -122,7 +122,7 @@ public class UserController {
 			model.addAttribute("listRoles", listRoles);
 
 			model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
-			return "user_form";
+			return "users/user_form";
 		} catch (UserNotFoundException ex) {
 			redirectAttributes.addFlashAttribute("message", ex.getMessage());
 			return "redirect:/users";

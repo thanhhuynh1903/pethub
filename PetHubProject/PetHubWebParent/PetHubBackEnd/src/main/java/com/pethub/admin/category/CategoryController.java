aa33d11b27d341c721848a7e88554250304dddcb
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-09-27 16:33:06 +0700

    Update Create Category

--- PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/category/CategoryController.java
+++ PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/category/CategoryController.java
@@ -6,8 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.pethub.admin.FileUploadUtil;
 import com.pethub.common.entity.Category;
 
 @Controller
@@ -16,9 +22,34 @@ public class CategoryController {
 	private CategoryService service;
 
 	@GetMapping("/categories")
-	public String listAll( Model model) {
+	public String listAll(Model model) {
 		List<Category> listCategories = service.listAll();
 		model.addAttribute("listCategories", listCategories);
 		return "categories/categories";
+	}
+
+	@GetMapping("/categories/new")
+	public String newCategory(Model model) {
+		List<Category> listCategories = service.listCategoriesUsedInForm();
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("category", new Category());
+		model.addAttribute("pageTitle", "Create New Category");// heading title
+
+		return "categories/category_form";
+	}
+
+	@PostMapping("/categories/save")
+	public String saveCategory(Category category, @RequestParam("fileImage") MultipartFile multipartFile,
+			RedirectAttributes ra) throws IOException {
+		List<Category> listCategories = service.listCategoriesUsedInForm();
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		category.setImage(fileName);
+
+		Category savedCategory = service.save(category);
+		String uploadDir = "../category-image/" + savedCategory.getId(); //the parent directory
+		FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
+		ra.addFlashAttribute("message","The category has been saved successfully.");
+		return "redirect:/categories";
 	}
 }

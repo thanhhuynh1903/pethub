5315676c1430159ce4722260e004f0e6871a7302
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-01 01:36:17 +0700

    Update Pagination/Filter/Sort

--- PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/brand/BrandRepository.java
+++ PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/brand/BrandRepository.java
@@ -1,5 +1,10 @@
 package com.pethub.admin.brand;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -10,4 +15,10 @@ public interface BrandRepository extends CrudRepository<Brand, Integer>, PagingA
 	public Long countById(Integer id);
 
 	public Brand findByName(String name);
+	
+	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+	public Page<Brand> findAll(String keyword, Pageable pageable);
+	
+	@Query("SELECT NEW Brand(b.id, b.name) FROM Brand b ORDER BY b.name ASC")
+	public List<Brand> findAll();
 }

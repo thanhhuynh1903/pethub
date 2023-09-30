83b29cec4b77f5c1c665693ff04a537bb61298f9
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-01 06:10:42 +0700

    Update Creating product overview

--- PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/product/ProductController.java
+++ PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/product/ProductController.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.pethub.admin.brand.BrandService;
+import com.pethub.common.entity.Brand;
 import com.pethub.common.entity.Product;
 
 @Controller
@@ -14,10 +19,39 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
+	@Autowired
+	private BrandService brandService;
+
 	@GetMapping("/products")
 	public String listAll(Model model) {
 		List<Product> listProducts = productService.listAll();
 		model.addAttribute("listProducts", listProducts);
 		return "products/products";
 	}
+
+	@GetMapping("/products/new")
+	public String newProduct(Model model) {
+		List<Brand> listBrands = brandService.listAll();
+		Product product = new Product();
+
+		product.setEnabled(true);
+		product.setInStock(true);
+
+		model.addAttribute("product", product);
+		model.addAttribute("listBrands", listBrands);
+		model.addAttribute("pageTitle", "Create New Product");
+
+		return "products/product_form";
+	}
+
+	@PostMapping("/products/save")
+	public String saveProduct(Product product) {
+		System.out.println("Product Name: " + product.getName());
+		System.out.println("Brand Id: " + product.getBrand().getId());
+
+		System.out.println("Category Id: " + product.getCategory().getId());
+
+		return "redirect:/products";
+	}
+
 }

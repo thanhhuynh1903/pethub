1f2e6c68c7de92c4971ffea028184a992dc1145f
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-01 21:55:37 +0700

    Update check Unique

--- PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/product/ProductRepository.java
+++ PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/product/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.pethub.admin.product;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -8,4 +10,11 @@ import com.pethub.common.entity.Product;
 public interface ProductRepository
 		extends PagingAndSortingRepository<Product, Integer>, CrudRepository<Product, Integer> {
 
+	public Product findByName(String name);
+
+	@Query("UPDATE Product p SET p.enabled = ?2 WHERE p.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, boolean enabled);
+
+	public Long countById(Integer id);
 }

191920c1e8ce3975e8aaa870eb2bf7b30cdc739d
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-02 05:14:19 +0700

    Create Product Details

--- PetHubProject/PetHubCommon/src/main/java/com/pethub/common/entity/Product.java
+++ PetHubProject/PetHubCommon/src/main/java/com/pethub/common/entity/Product.java
@@ -1,7 +1,9 @@
 package com.pethub.common.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -74,6 +76,9 @@ public class Product {
 	// persited as well
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
 	private Set<ProductImage> images = new HashSet<>();
+
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private List<ProductDetail> details = new ArrayList<>();
 
 	public Product() {
 	}
@@ -253,5 +258,17 @@ public class Product {
 			return "/images/image-thumbnail.png";
 		}
 		return "/product-images/" + this.id + "/" + this.mainImage;
+	}
+
+	public List<ProductDetail> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<ProductDetail> details) {
+		this.details = details;
+	}
+
+	public void addDetail(String name, String value) {
+		this.details.add(new ProductDetail(name, value, this));
 	}
 }

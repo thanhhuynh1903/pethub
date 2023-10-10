fe8754761d54851ce03485a3ac143088b413be8d
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-10 14:48:43 +0700

    Update sương sương để đi ngủ

---
+++ PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/setting/SettingRepository.java
@@ -0,0 +1,19 @@
+package com.pethub.setting;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.pethub.common.entity.Setting;
+import com.pethub.common.entity.SettingCategory;
+
+
+public interface SettingRepository extends CrudRepository<Setting, String> {
+	public List<Setting> findByCategory(SettingCategory category);
+	
+	@Query("SELECT s FROM Setting s WHERE s.category = ?1 OR s.category = ?2")
+	public List<Setting> findByTwoCategories(SettingCategory catOne, SettingCategory catTwo);
+	
+	public Setting findByKey(String key);
+}

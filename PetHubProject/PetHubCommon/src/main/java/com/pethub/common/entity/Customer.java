06d1aa037a0821640dc0d2815a47cd7dd23a87fa
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-14 10:35:58 +0700

    Update send mail verification

--- PetHubProject/PetHubCommon/src/main/java/com/pethub/common/entity/Customer.java
+++ PetHubProject/PetHubCommon/src/main/java/com/pethub/common/entity/Customer.java
@@ -2,18 +2,14 @@ package com.pethub.common.entity;
 
 import java.util.Date;
 
-import jakarta.annotation.Resource.AuthenticationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "customers")
@@ -213,6 +209,7 @@ public class Customer {
 	public void setCountry(Country country) {
 		this.country = country;
 	}
+	
 
 	@Override
 	public String toString() {

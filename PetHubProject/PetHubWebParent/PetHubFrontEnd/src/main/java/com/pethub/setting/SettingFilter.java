c9e29636e6ec94fe8fb5b65780ef81540243f388
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-15 19:30:44 +0700

    Update logout and shopping after login interface

--- PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/setting/SettingFilter.java
+++ PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/setting/SettingFilter.java
@@ -38,10 +38,7 @@ public class SettingFilter implements Filter {
 		List<Setting> generalSettings = service.getGeneralSettings();
 
 		generalSettings.forEach(setting -> {
-			System.out.println(setting);
 			request.setAttribute(setting.getKey(), setting.getValue());
-			System.out.println(setting.getKey() + " == > " + setting.getValue());
-
 		});
 
 		chain.doFilter(request, response);

3162f4e042d2969bd8e5c5a86c801590dc14052d
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-15 19:18:29 +0700

    Update Login page and config security

--- PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/MainController.java
+++ PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/MainController.java
@@ -3,6 +3,9 @@ package com.pethub;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +21,19 @@ public class MainController {
 
 	@GetMapping("")
 	public String viewHomePage(Model model) {
-	List<Category> listCategories =	categoryService.listNoChildrenCategories();
-		
-	model.addAttribute("listCategories",listCategories);
-	return "index";
+		List<Category> listCategories = categoryService.listNoChildrenCategories();
+
+		model.addAttribute("listCategories", listCategories);
+		return "index";
+	}
+
+	@GetMapping("/login")
+	public String viewLoginPage() {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+			return "login";
+		}
+		return "redirect:/";
 	}
 }

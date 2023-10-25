a22c0a5420f938018b53fedcb6c99e31d52960db
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-26 02:35:04 +0700

    Update entity

--- PetHubProject/PetHubCommon/src/main/java/com/pethub/common/entity/Customer.java
+++ PetHubProject/PetHubCommon/src/main/java/com/pethub/common/entity/Customer.java
@@ -6,20 +6,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "customers")
-public class Customer {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+public class Customer extends IdBasedEntity {
 
 	@Column(nullable = false, unique = true, length = 45)
 	private String email;
@@ -81,14 +74,6 @@ public class Customer {
 	public Customer(Integer id, String email) {
 		this.id = id;
 		this.email = email;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
 	}
 
 	public String getEmail() {

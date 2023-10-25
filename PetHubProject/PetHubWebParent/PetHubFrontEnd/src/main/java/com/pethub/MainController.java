d702c63d19b0f5d8ed4026feebc926d7629ea679
ngockim109 <ngockim9047@gmail.com>
2023-10-25 17:33:54 +0700

    add star_rating, sold in component product of product by category, responsive

--- PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/MainController.java
+++ PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/MainController.java
@@ -24,7 +24,7 @@ public class MainController {
 		List<Category> listCategories = categoryService.listNoChildrenCategories();
 
 		model.addAttribute("listCategories", listCategories);
-		return "home";
+		return "index";
 	}
 
 	@GetMapping("/login")

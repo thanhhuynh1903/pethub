6630e3772732d50c75aae71930fb261214ad5cec
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-28 20:58:01 +0700

    Fix bug
    Use @RequestParam when:
    - Set default value
    - Param is not required
    - Param name is different than argument name

--- PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/user/controller/UserRestController.java
+++ PetHubProject/PetHubWebParent/PetHubBackEnd/src/main/java/com/pethub/admin/user/controller/UserRestController.java
@@ -2,7 +2,6 @@ package com.pethub.admin.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pethub.admin.user.UserService;
@@ -13,7 +12,7 @@ public class UserRestController {
 	private UserService service;
 
 	@PostMapping("/users/check_email")
-	public String checkDuplicateEmail(@RequestParam("id") Integer id, @RequestParam("email") String email) {
+	public String checkDuplicateEmail(Integer id, String email) {
 		return service.isEmailUnique(id, email) ? "OK" : "Duplicated";
 	}
 }

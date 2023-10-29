26ea661afa20239993ceb25f13c2a3fae6a7a892
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-29 18:59:11 +0700

    Update addressbook

--- PetHubProject/PetHubCommon/src/main/java/com/pethub/common/entity/Customer.java
+++ PetHubProject/PetHubCommon/src/main/java/com/pethub/common/entity/Customer.java
@@ -6,43 +6,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "customers")
-public class Customer extends IdBasedEntity {
+public class Customer extends AbstractAddressWithCountry {
 
 	@Column(nullable = false, unique = true, length = 45)
 	private String email;
 
 	@Column(nullable = false, length = 64)
 	private String password;
-
-	@Column(name = "first_name", nullable = false, length = 45)
-	private String firstName;
-
-	@Column(name = "last_name", nullable = false, length = 45)
-	private String lastName;
-
-	@Column(name = "phone_number", nullable = false, length = 15)
-	private String phoneNumber;
-
-	@Column(nullable = false, length = 64)
-	private String addressLine1;
-
-	@Column(name = "address_line_2", length = 64)
-	private String addressLine2;
-
-	@Column(nullable = false, length = 45)
-	private String city;
-
-	@Column(nullable = false, length = 45)
-	private String state;
-
-	@Column(name = "postal_code", nullable = false, length = 10)
-	private String postalCode;
 
 	@Column(name = "verification_code", length = 64)
 	private String verificationCode;
@@ -52,10 +26,6 @@ public class Customer extends IdBasedEntity {
 	@Column(name = "created_time")
 	private Date createdTime;
 
-	@ManyToOne
-	@JoinColumn(name = "country_id")
-	private Country country;
-
 	@Enumerated(EnumType.STRING)
 	@Column(name = "authentication_type", length = 10)
 	private AuthenticationType authenticationType;
@@ -64,16 +34,10 @@ public class Customer extends IdBasedEntity {
 	private String resetPasswordToken;
 
 	public Customer() {
-
 	}
 
 	public Customer(Integer id) {
 		this.id = id;
-	}
-
-	public Customer(Integer id, String email) {
-		this.id = id;
-		this.email = email;
 	}
 
 	public String getEmail() {
@@ -90,70 +54,6 @@ public class Customer extends IdBasedEntity {
 
 	public void setPassword(String password) {
 		this.password = password;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getAddressLine1() {
-		return addressLine1;
-	}
-
-	public void setAddressLine1(String addressLine1) {
-		this.addressLine1 = addressLine1;
-	}
-
-	public String getAddressLine2() {
-		return addressLine2;
-	}
-
-	public void setAddressLine2(String addressLine2) {
-		this.addressLine2 = addressLine2;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getPostalCode() {
-		return postalCode;
-	}
-
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
 	}
 
 	public String getVerificationCode() {
@@ -178,23 +78,6 @@ public class Customer extends IdBasedEntity {
 
 	public void setCreatedTime(Date createdTime) {
 		this.createdTime = createdTime;
-	}
-
-	public Country getCountry() {
-		return country;
-	}
-
-	public void setCountry(Country country) {
-		this.country = country;
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", email=" + email + ", password=" + password + ", firstName=" + firstName
-				+ ", lastName=" + lastName + ", phoneNumber=" + phoneNumber + ", addressLine1=" + addressLine1
-				+ ", addressLine2=" + addressLine2 + ", city=" + city + ", state=" + state + ", postalCode="
-				+ postalCode + ", verificationCode=" + verificationCode + ", enabled=" + enabled + ", createdTime="
-				+ createdTime + ", country=" + country + "]";
 	}
 
 	public String getFullName() {

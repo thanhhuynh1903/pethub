130aef0f24b16b18ff5cbb0d117ef59f49f70f14
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-10-31 17:14:59 +0700

    Update Place Order with COD and confirmation email

--- PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/Utility.java
+++ PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/Utility.java
@@ -1,5 +1,7 @@
 package com.pethub;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Properties;
 
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -8,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
 import com.pethub.security.oauth.CustomerOAuth2User;
+import com.pethub.setting.CurrencySettingBag;
 import com.pethub.setting.EmailSettingBag;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -53,6 +56,36 @@ public class Utility {
 		}
 
 		return customerEmail;
+	}
+
+	public static String formatCurrency(float amount, CurrencySettingBag settings) {
+		String symbol = settings.getSymbol();
+		String symbolPosition = settings.getSymbolPosition();
+		String decimalPointType = settings.getDecimalPointType();
+		String thousandPointType = settings.getThousandPointType();
+		int decimalDigits = settings.getDecimalDigits();
+
+		String pattern = symbolPosition.equals("Before price") ? symbol : "";
+		pattern += "###,###";
+
+		if (decimalDigits > 0) {
+			pattern += ".";
+			for (int count = 1; count <= decimalDigits; count++)
+				pattern += "#";
+		}
+
+		pattern += symbolPosition.equals("After price") ? symbol : "";
+
+		char thousandSeparator = thousandPointType.equals("POINT") ? '.' : ',';
+		char decimalSeparator = decimalPointType.equals("POINT") ? '.' : ',';
+
+		DecimalFormatSymbols decimalFormatSymbols = DecimalFormatSymbols.getInstance();
+		decimalFormatSymbols.setDecimalSeparator(decimalSeparator);
+		decimalFormatSymbols.setGroupingSeparator(thousandSeparator);
+
+		DecimalFormat formatter = new DecimalFormat(pattern, decimalFormatSymbols);
+
+		return formatter.format(amount);
 	}
 
 }
--- PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/setting/SettingService.java
+++ PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/setting/SettingService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pethub.common.entity.Currency;
 import com.pethub.common.entity.setting.Setting;
 import com.pethub.common.entity.setting.SettingCategory;
 
@@ -24,4 +25,15 @@ public class SettingService {
 
 		return new EmailSettingBag(settings);
 	}
+
+	public CurrencySettingBag getCurrencySettings() {
+		List<Setting> settings = settingRepo.findByCategory(SettingCategory.CURRENCY);
+		return new CurrencySettingBag(settings);
+	}
+
+	public PaymentSettingBag getPaymentSettings() {
+		List<Setting> settings = settingRepo.findByCategory(SettingCategory.PAYMENT);
+		return new PaymentSettingBag(settings);
+	}
+
 }

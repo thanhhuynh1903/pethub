f8c75365d5acc151f33af05cf364abebf4840b61
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-11-05 05:31:00 +0700

    update view order for customer

--- PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/order/OrderService.java
+++ PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/order/OrderService.java
@@ -21,25 +21,25 @@ import com.pethub.common.entity.order.OrderStatus;
 import com.pethub.common.entity.order.OrderTrack;
 import com.pethub.common.entity.order.PaymentMethod;
 import com.pethub.common.entity.product.Product;
+import com.pethub.common.exception.OrderNotFoundException;
 
 @Service
 public class OrderService {
 	public static final int ORDERS_PER_PAGE = 5;
-
-	@Autowired
-	private OrderRepository repo;
-
-	public Order createOrder(Customer customer, Address address, List<CartItem> cartItems, PaymentMethod paymentMethod,
-			CheckoutInfo checkoutInfo) {
+	
+	@Autowired private OrderRepository repo;
+	
+	public Order createOrder(Customer customer, Address address, List<CartItem> cartItems,
+			PaymentMethod paymentMethod, CheckoutInfo checkoutInfo) {
 		Order newOrder = new Order();
 		newOrder.setOrderTime(new Date());
-
+		
 		if (paymentMethod.equals(PaymentMethod.PAYPAL)) {
 			newOrder.setStatus(OrderStatus.PAID);
 		} else {
 			newOrder.setStatus(OrderStatus.NEW);
 		}
-
+		
 		newOrder.setCustomer(customer);
 		newOrder.setProductCost(checkoutInfo.getProductCost());
 		newOrder.setSubtotal(checkoutInfo.getProductTotal());
@@ -49,18 +49,18 @@ public class OrderService {
 		newOrder.setPaymentMethod(paymentMethod);
 		newOrder.setDeliverDays(checkoutInfo.getDeliverDays());
 		newOrder.setDeliverDate(checkoutInfo.getDeliverDate());
-
+		
 		if (address == null) {
 			newOrder.copyAddressFromCustomer();
 		} else {
 			newOrder.copyShippingAddress(address);
 		}
-
+		
 		Set<OrderDetail> orderDetails = newOrder.getOrderDetails();
-
+		
 		for (CartItem cartItem : cartItems) {
 			Product product = cartItem.getProduct();
-
+			
 			OrderDetail orderDetail = new OrderDetail();
 			orderDetail.setOrder(newOrder);
 			orderDetail.setProduct(product);
@@ -69,37 +69,63 @@ public class OrderService {
 			orderDetail.setProductCost(product.getCost() * cartItem.getQuantity());
 			orderDetail.setSubtotal(cartItem.getSubtotal());
 			orderDetail.setShippingCost(cartItem.getShippingCost());
-
+			
 			orderDetails.add(orderDetail);
 		}
-
+		
 		OrderTrack track = new OrderTrack();
 		track.setOrder(newOrder);
 		track.setStatus(OrderStatus.NEW);
 		track.setNotes(OrderStatus.NEW.defaultDescription());
 		track.setUpdatedTime(new Date());
-
+		
 		newOrder.getOrderTracks().add(track);
-
+		
 		return repo.save(newOrder);
 	}
-
-	public Page<Order> listForCustomerByPage(Customer customer, int pageNum, String sortField, String sortDir,
-			String keyword) {
+	
+	public Page<Order> listForCustomerByPage(Customer customer, int pageNum, 
+			String sortField, String sortDir, String keyword) {
 		Sort sort = Sort.by(sortField);
 		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
-
+		
 		Pageable pageable = PageRequest.of(pageNum - 1, ORDERS_PER_PAGE, sort);
-
+		
 		if (keyword != null) {
 			return repo.findAll(keyword, customer.getId(), pageable);
 		}
-
+		
 		return repo.findAll(customer.getId(), pageable);
 	}
-
+	
 	public Order getOrder(Integer id, Customer customer) {
 		return repo.findByIdAndCustomer(id, customer);
+	}	
+	
+	public void setOrderReturnRequested(OrderReturnRequest request, Customer customer) 
+			throws OrderNotFoundException {
+		Order order = repo.findByIdAndCustomer(request.getOrderId(), customer);
+		if (order == null) {
+			throw new OrderNotFoundException("Order ID " + request.getOrderId() + " not found");
+		}
+		
+		if (order.isReturnRequested()) return;
+		
+		OrderTrack track = new OrderTrack();
+		track.setOrder(order);
+		track.setUpdatedTime(new Date());
+		track.setStatus(OrderStatus.RETURN_REQUESTED);
+		
+		String notes = "Reason: " + request.getReason();
+		if (!"".equals(request.getNote())) {
+			notes += ". " + request.getNote();
+		}
+		
+		track.setNotes(notes);
+		
+		order.getOrderTracks().add(track);
+		order.setStatus(OrderStatus.RETURN_REQUESTED);
+		
+		repo.save(order);
 	}
-
 }

413e38fe6be42d5e961410f8810ad0c748d525a8
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2023-11-07 01:33:41 +0700

    Update Review Management Customer

---
+++ PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/review/ReviewRepository.java
@@ -0,0 +1,32 @@
+package com.pethub.review;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.pethub.common.entity.Review;
+import com.pethub.common.entity.product.Product;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+	
+	@Query("SELECT r FROM Review r WHERE r.customer.id = ?1")
+	public Page<Review> findByCustomer(Integer customerId, Pageable pageable);
+	
+	@Query("SELECT r FROM Review r WHERE r.customer.id = ?1 AND ("
+			+ "r.headline LIKE %?2% OR r.comment LIKE %?2% OR "
+			+ "r.product.name LIKE %?2%)")
+	public Page<Review> findByCustomer(Integer customerId, String keyword, Pageable pageable);
+	
+	@Query("SELECT r FROM Review r WHERE r.customer.id = ?1 AND r.id = ?2")
+	public Review findByCustomerAndId(Integer customerId, Integer reviewId);
+	
+	public Page<Review> findByProduct(Product product, Pageable pageable);
+	
+	@Query("SELECT COUNT(r.id) FROM Review r WHERE r.customer.id = ?1 AND "
+			+ "r.product.id = ?2")
+	public Long countByCustomerAndProduct(Integer customerId, Integer productId);
+	
+	
+}

3cdabcd795fd74907ce42bd2d33c1dc5760a6adf
ngockim109 <ngockim9047@gmail.com>
2023-11-08 20:28:41 +0700

    resolve conflict main

--- PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/brand/BrandRepository.java
+++ PetHubProject/PetHubWebParent/PetHubFrontEnd/src/main/java/com/pethub/brand/BrandRepository.java
@@ -6,7 +6,7 @@ import com.pethub.common.entity.Brand;
 import com.pethub.common.entity.Category;
 
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
-  public List<Brand> findByCategoriesContaining(Category category);
+//  public List<Brand> findByCategoriesContaining(Category category);
 
   public Brand findByName(String name);
 
